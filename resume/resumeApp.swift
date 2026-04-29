320424b87b83d6c62ba33a79aa3cbeb8dfb76f59
firsec <toget@qq.com>
2026-04-28 17:19:11 -0700

    init repo

---
+++ resume/resumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  resumeApp.swift
+//  resume
+//
+//  Created by HY on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct resumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
